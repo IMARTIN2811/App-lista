a630626284a5839cee57bb872e680143d513bdcb
Isra <israel_2896@hotmail.com>
2020-07-20 12:53:07 -0500

    Se codificó el css, el js y el form

---
+++ js/app.js
@@ -0,0 +1,25 @@
+(function() {
+    //accede al id del elemento html
+    var lista = document.getElementById("lista"),
+    tareaInput = document.getElementById("tareaInput"),
+    btnAgregar = document.getElementById("btn-agregar");
+
+    //funcion
+    var agregarTarea = function(){
+
+}
+
+    var comprobarInput = function() {
+        
+}
+
+    //Se crea el Evento de los input
+    btnAgregar.addEventListener("click",agregarTarea);
+    tareaInput.addEventListener("click", comprobarInput);
+
+    //recorre todos los elementos li de la lista
+    for (var i = 0; i <= lista.children.length; i++) {
+        
+    }
+
+}());
